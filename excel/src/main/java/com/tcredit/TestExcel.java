ea53e5629f608b54611f9ad951a0dc255ac7c905
chonghuo <chobg.huo@tcredit.com>
2019-01-06 09:36:42 +0800

    000

--- excel/src/main/java/com/tcredit/TestExcel.java
+++ excel/src/main/java/com/tcredit/TestExcel.java
@@ -6,15 +6,15 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 
 /**
  * Hello world!
  */
-public class TestExcel {
-    public static void main(String[] args) {
+public class TestExcel<main> {
+
+
+    public static void main1(String[] args) {
         //定义标题
         String[] title = {"订单号","订单ID","订单状态"};
         //创建excel工作簿
@@ -46,8 +46,54 @@ public class TestExcel {
             file.createNewFile();
             FileOutputStream fos = FileUtils.openOutputStream(file);
             book.write(fos);
+//            byte[] bytes =book.getBytes();
+//            fos.write(bytes);
+
             fos.close();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static void main2(String[] args) {
+
+        File  file  = new File("aaa/bbb/p2p.txt");
+        FileInputStream fis =null;
+        byte[] bytes = new byte[1024];
+        try {
+            fis = FileUtils.openInputStream(file);
+
+            while(fis.read(bytes) !=-1){
+                System.out.println(new String(bytes));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis !=null){
+                    fis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        File file  = new File("aaa/bbb/bbb.txt");
+
+        String str = "aaaaaaaaaa ";
+        byte[] bytes = str.getBytes();
+        try {
+            FileOutputStream fos = new FileOutputStream(file,true);
+            fos.write(bytes);
+            fos.flush();
+            fos.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
